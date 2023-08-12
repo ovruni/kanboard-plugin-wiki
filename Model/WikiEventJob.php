02f8227d230bf8925ce272c2f935b9e5fd7122fe
Omar Vega Ramos <ovruni@localhost.localdomain>
2023-08-11 23:47:18 -0500

    Modifying order of parameters in the dispatch method

--- Model/WikiEventJob.php
+++ Model/WikiEventJob.php
@@ -39,7 +39,7 @@ class WikiEventJob extends BaseJob
             ->buildEventWiki($wikiPage);
 
         if ($event !== null) {
-            $this->dispatcher->dispatch($eventName, $event);
+            $this->dispatcher->dispatch($event, $eventName);
 
             // if ($eventName === Wiki::EVENT_CREATE) {
             //     $userMentionJob = $this->userMentionJob->withParams($event['comment']['comment'], Wiki::EVENT_USER_MENTION, $event);
@@ -55,7 +55,7 @@ class WikiEventJob extends BaseJob
             ->buildEvent();
 
         if ($event !== null) {
-            $this->dispatcher->dispatch($eventName, $event);
+            $this->dispatcher->dispatch($event, $eventName);
 
             // if ($eventName === Wiki::EVENT_CREATE) {
             //     $userMentionJob = $this->userMentionJob->withParams($event['comment']['comment'], Wiki::EVENT_USER_MENTION, $event);
